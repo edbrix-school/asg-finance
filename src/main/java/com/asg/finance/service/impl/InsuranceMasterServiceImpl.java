0811d216f43ba451bf2061fa3dc1240aa017ad2b
Harsh Taneja <htaneja@hexalytics.com>
2026-02-06 11:17:05 +0530

    Fix

--- src/main/java/com/asg/finance/service/impl/InsuranceMasterServiceImpl.java
+++ src/main/java/com/asg/finance/service/impl/InsuranceMasterServiceImpl.java
@@ -278,6 +278,8 @@ public class InsuranceMasterServiceImpl implements InsuranceMasterService {
         existing.setInsuranceProvider(request.getInsuranceProvider());
         existing.setFromDate(request.getFromDate());
         existing.setExpiryDate(request.getExpiryDate());
+        existing.setCurrencyPoid(request.getCurrency());
+        existing.setExchangeRate(request.getRate());
         existing.setInsuranceAmount(request.getInsuranceAmount());
         existing.setPremiumAmount(request.getPremiumAmount());
         existing.setPaymentFrequency(request.getPaymentFrequency());
@@ -647,7 +649,7 @@ public class InsuranceMasterServiceImpl implements InsuranceMasterService {
     public DetailsDto getRoleDetails(Long rolePoid) {
         RoleDto roleDto = roleServiceClient.findById(rolePoid);
         if (roleDto == null) {
-            throw new ResourceNotFoundException("Global User Role", "userRolePoid", rolePoid);
+            throw new ValidationException("Please select a valid User Role in PIC Details");
         }
         return new DetailsDto(
                 roleDto.getUserRolePoid(),     // poid

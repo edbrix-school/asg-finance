3edc03c820652f7fc3cd45b275e949076732ecf4
AKhilesh-Dwivedi <adwivedi@hexalytics.com>
2026-02-10 17:09:48 +0530

    implement button in purchase journal load item details procedure

--- src/main/java/com/asg/finance/service/impl/ApPurchaseJournalServiceImpl.java
+++ src/main/java/com/asg/finance/service/impl/ApPurchaseJournalServiceImpl.java
@@ -1744,8 +1744,8 @@ public class ApPurchaseJournalServiceImpl implements ApPurchaseServiceJournal {
         List<ApPiFromPoResponseDto> list =
                 apPurchaseJournalRepositoryImpl.createPiFromPo(
                         UserContext.getGroupPoid(),
-                        UserContext.getUserPoid(),
                         UserContext.getCompanyPoid(),
+                        UserContext.getUserPoid(),
                         poPoid, result);
 
         log.info("Result from PROC_AP_PI_CREATE_FROM_PO → {}", result);

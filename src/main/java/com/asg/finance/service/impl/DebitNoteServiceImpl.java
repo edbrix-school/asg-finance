66ee0b7f41bfee8bbb557a122d47a965ebb61fa5
ksharma321 <ksharma@hexalytics.com>
2026-02-20 16:45:03 +0530

    fix debit note issue

--- src/main/java/com/asg/finance/service/impl/DebitNoteServiceImpl.java
+++ src/main/java/com/asg/finance/service/impl/DebitNoteServiceImpl.java
@@ -145,10 +145,11 @@ public class DebitNoteServiceImpl implements DebitNoteService {
         existingEntity.setMultiCompany(debitNoteDto.getMultiCompany() != null && debitNoteDto.getMultiCompany() ? "Y" : "N");
         existingEntity.setRemarksPrintable(debitNoteDto.getRemarksPrintable() != null && debitNoteDto.getRemarksPrintable() ? "Y" : "N");
         existingEntity.setShowBankDetailsInPrint(debitNoteDto.getShowBankDetailsInPrint() != null && debitNoteDto.getShowBankDetailsInPrint() ? "Y" : "N");
-
         existingEntity.setGroupPoid(UserContext.getGroupPoid());
         existingEntity.setCompanyPoid(UserContext.getCompanyPoid());
         existingEntity.setOtherCurrAmount(debitNoteDto.getOtherCurrAmount());
+        existingEntity.setFdaRef(debitNoteDto.getFdaRefPoid().toString());
+        existingEntity.setFdaDirectRef(debitNoteDto.getFdaDirectRefPoid().toString());
 
         existingEntity.setLastModifiedBy(ASGHelperUtils.getCurrentUser());
         existingEntity.setLastModifiedDate(LocalDateTime.now());

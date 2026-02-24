c5ce1cf50cbe5e17715b100dbbef369c576fd470
Sahil Khurana <skhurana@hexalytics.com>
2026-02-24 19:57:18 +0530

    LOV Fixed

--- src/main/java/com/asg/finance/service/impl/CreditNoteServiceImpl.java
+++ src/main/java/com/asg/finance/service/impl/CreditNoteServiceImpl.java
@@ -385,7 +385,7 @@ public class CreditNoteServiceImpl implements CreditNoteService {
                     dto.setRemarks(rs.getString("REMARKS"));
                     dto.setIssueInvoice("N");
 
-                    dto.setChargeDet(lovService.getDetailsByPoidAndLovNameFast(dto.getChargePoid(), "CHARGE_MASTER"));
+                    dto.setChargeDet(lovService.getDetailsByPoidAndLovNameFast(dto.getChargePoid(), "CHARGE_MASTER_ALL"));
                     dto.setTaxDet(taxMasterRepository.findByTaxPoid(dto.getTaxPoid())
                             .map(tm -> new LovGetListDto(tm.getTaxPoid(), tm.getTaxCode(), tm.getTaxName(), tm.getTaxPoid(), tm.getTaxName(), tm.getSeqNo(), null))
                             .orElse(null));

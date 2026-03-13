57fd61a0cfda1326144b9b274fe5b34b7f86cbaf
ksharma321 <ksharma@hexalytics.com>
2026-03-13 20:03:01 +0530

    fix/add-gl-errors

--- src/main/java/com/asg/finance/aspect/GlPostingAspect.java
+++ src/main/java/com/asg/finance/aspect/GlPostingAspect.java
@@ -1,10 +1,12 @@
 package com.asg.finance.aspect;
 
+import com.asg.common.lib.exception.ValidationException;
 import com.asg.common.lib.security.util.UserContext;
 import com.asg.finance.annotation.PerformGlPosting;
 import com.asg.finance.service.GlPostingService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.catalina.User;
 import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
@@ -36,20 +38,23 @@ public class GlPostingAspect {
 
             Long transactionPoid = extractLongValue(result, "getPoid", "getTransactionPoid");
             String docRef = extractStringValue(result, "getDocRef");
-
+            String postingResult = null;
             if (transactionPoid != null && StringUtils.hasText(docId)) {
-                glPostingService.performGlPosting(docId, transactionPoid, docRef);
+                postingResult = glPostingService.performGlPosting(docId, transactionPoid, docRef);
             } else {
                 log.warn("PerformGlPosting: Could not extract necessary information. DocId: {}, Poid: {}, DocRef: {}", 
                         docId, transactionPoid, docRef);
             }
 
+            if (postingResult != null && postingResult.contains("ERROR")) {
+                log.error("GL posting failed: {}", result);
+                UserContext.setGlPostingError("GL Posting failed: " + result);
+            }
+
+
         } catch (Exception e) {
             log.error("Error in GlPostingAspect: {}", e.getMessage(), e);
-            if (e instanceof RuntimeException) {
-                throw e;
-            }
-            throw new RuntimeException("Unexpected error during automatic GL Posting", e);
+            UserContext.setGlPostingError(String.format("Unexpected error during automatic GL Posting: %s", e.getMessage()));
         }
     }
 

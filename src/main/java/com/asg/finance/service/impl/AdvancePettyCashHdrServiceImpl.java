a844d727f67c747956caff35f29d560b1bb05185
ksharma321 <ksharma@hexalytics.com>
2026-04-13 14:40:35 +0530

    fix-advance-petty cash

--- src/main/java/com/asg/finance/service/impl/AdvancePettyCashHdrServiceImpl.java
+++ src/main/java/com/asg/finance/service/impl/AdvancePettyCashHdrServiceImpl.java
@@ -61,6 +61,7 @@ public class AdvancePettyCashHdrServiceImpl implements AdvancePettyCashHdrServic
     private final DocumentDeleteService documentDeleteService;
 
     @Override
+    @Transactional
     public AdvancePettyCashHdrResponseDTO createAdvancePettyCash(AdvancePettyCashHdrRequestDTO request) {
         validateTransactionDate(request.getTransactionDate());
         validateClosedStatus(request.getStatus(), request.getClosedReason());
@@ -85,6 +86,7 @@ public class AdvancePettyCashHdrServiceImpl implements AdvancePettyCashHdrServic
     }
 
     @Override
+    @Transactional
     public AdvancePettyCashHdrResponseDTO updateAdvancePettyCash(Long transactionPoid, AdvancePettyCashHdrRequestDTO request) {
         AdvancePettyCashHdr existing = repository.findByTransactionPoid(transactionPoid)
                 .orElseThrow(() -> new ResourceNotFoundException("Advance Petty Cash not found with ID: ", "transactionPoid", transactionPoid));

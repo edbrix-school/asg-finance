3648ba06cadf1840446bfe0329ab880bd3ce7b18
Sridharan-Official <srajendaran@hexalytics.com>
2026-04-13 10:52:35 +0530

    Added the docment title in the view api

--- src/main/java/com/asg/finance/dto/BankReconciliationResponse.java
+++ src/main/java/com/asg/finance/dto/BankReconciliationResponse.java
@@ -1,24 +1,25 @@
 package com.asg.finance.dto;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-import lombok.Data;
-
 @Data
 public class BankReconciliationResponse {
-	private Long transactionGroupPoid;
-	private Long transactionCompanyPoid;
-	private String docId; 
-	private String docId1; 
-	private Long transactionPoid; 
-	private LocalDate transactionDate; 
-	private String docRef; 
-	private String chequeRef;
-	private Long detRowId;
-	private String narration;
-	private Long glCompanyPoid;
-	private Long glPoid;
-	private BigDecimal crAmt;
-	private BigDecimal drAmt; 
+    private Long transactionGroupPoid;
+    private Long transactionCompanyPoid;
+    private String docId;
+    private String docId1;
+    private String docTitle;
+    private Long transactionPoid;
+    private LocalDate transactionDate;
+    private String docRef;
+    private String chequeRef;
+    private Long detRowId;
+    private String narration;
+    private Long glCompanyPoid;
+    private Long glPoid;
+    private BigDecimal crAmt;
+    private BigDecimal drAmt;
 }
